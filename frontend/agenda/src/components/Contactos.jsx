426eb21ad17ab1441287e6ae582b6b89b171b6a6
Alvaro-SP <3034161730108@ingenieria.usac.edu.gt>
2023-02-22 00:27:19 -0600

    Se enlazaron los componentes y se testeo la app
    Se han enlazado los componentes y se ha revisado la conexion de backend y frontend para comprobar las funcionalidades esperadas, y asi tener en revision todas las funcionalidades realizadas en las ramas anteriores.

--- frontend/agenda/src/components/Contactos.jsx
+++ frontend/agenda/src/components/Contactos.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import '../styles/Contactos.css'
+
+import {AddContact} from './AddContact'
+import ListaContacto from './ListaContacto'
 function Contactos() {
     return (
         <div className="contenedor-general">
-            <h1>Componente de contactos</h1>
+            <h1>Contactos Guardados</h1>
+            <AddContact/>
+            <ListaContacto/>
         </div>
     )
 }
